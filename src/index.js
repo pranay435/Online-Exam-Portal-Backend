bbfb87642b422cecf932a75d0b4b291f40717a35
pranay435 <pranaykumar812003@gmail.com>
2024-08-11 21:20:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,18 @@ require('./config/env');
 
 // Middleware setup
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,POST',
-    allowedHeaders: 'Content-Type,Authorization'
-}));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     methods: 'GET,POST',
+//     allowedHeaders: 'Content-Type,Authorization'
+// }));
 
 // Route setup
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/test', require('./routes/testRoutes'));
+app.get('/',(req,res)=>{
+    console.log("server is running...")
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
