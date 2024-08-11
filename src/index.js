864a5338fc6c83fecb191a07be85c671eded7c6f
pranay435 <pranaykumar812003@gmail.com>
2024-08-11 21:27:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get('/',(req,res)=>{
     res.send("backend is running...")
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is up and running on ${PORT}`);
 });
