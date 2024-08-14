76f4f092768d172db199b638086a424f62cf30b5
pranay435 <pranaykumar812003@gmail.com>
2024-08-15 01:08:59 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,26 @@
-const mysql = require("mysql");
-require("./env")
+const mysql = require('mysql');
+const fs = require('fs');
 
-const client = mysql.createConnection({
+// Read the certificate file
+const serverCert = fs.readFileSync('./server-cert.pem');
+
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-      ssl: {
-        rejectUnauthorized: false
+    ssl: {
+        ca: serverCert,
     }
 });
 
-client.connect((err) => {
-    if (err) throw err;
-    console.log("Successfully connected to the database...");
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Connected as id ' + connection.threadId);
 });
 
-module.exports = client;
+module.exports = connection;
